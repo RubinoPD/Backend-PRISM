ffc10c0746d54f70d91d7b932437134811689cf4
RubinoPD <robertaskask@gmail.com>
2025-05-20 14:47:54 +0300

    Add taskSchema

---
+++ src/models/task.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ["Individualios", "Kolektyvines"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Task", taskSchema);
