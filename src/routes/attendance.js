79f365b99d445e9fa02e17f7a9570936f730476e
RubinoPD <robertaskask@gmail.com>
2025-05-20 16:25:08 +0300

    Added attendance route

---
+++ src/routes/attendance.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const { protect, adminOnly, adminOrSuperuser } = require("../middleware/auth");
+const {
+  getAllAttendance,
+  getAttendanceByDate,
+  createAttendance,
+  bulkCreateAttendance,
+  updateAttendance,
+  deleteAttendance,
+  getAttendanceStats,
+} = require("../controllers/attendance");
+
+// All routes are protected
+router.use(protect);
+
+// Routes accessible by all authenticated users
+router.get("/", getAllAttendance);
+router.get("/stats", getAttendanceStats);
+router.get("/date/:date", getAttendanceByDate);
+
+// Routes accessible by admin and superuser
+router.post("/", adminOrSuperuser, createAttendance);
+router.post("/bulk", adminOrSuperuser, bulkCreateAttendance);
+router.put("/:id", adminOrSuperuser, updateAttendance);
+
+// Routes accessible only by admin
+router.delete("/:id", adminOnly, deleteAttendance);
+
+module.exports = router;
