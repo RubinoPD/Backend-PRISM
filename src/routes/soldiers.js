1a3974680775bee1b3d3e7232b6f15b637173304
RubinoPD <robertaskask@gmail.com>
2025-12-09 15:04:36 +0200

    Updated to allow superuser to delete soldiers

--- src/routes/soldiers.js
+++ src/routes/soldiers.js
@@ -1,24 +1,24 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { protect, adminOnly, adminOrSuperuser } = require("../middleware/auth");
+const { protect, adminOnly, adminOrSuperuser } = require('../middleware/auth');
 const {
   getAllSoldiers,
   getSoldierById,
   createSoldier,
   updateSoldier,
   deleteSoldier,
-} = require("../controllers/soldiers");
+} = require('../controllers/soldiers');
 
 // All routes are protected
 router.use(protect);
 
 // Routes accessible by admin and superuser
-router.get("/", getAllSoldiers);
-router.get("/:id", getSoldierById);
-router.post("/", adminOrSuperuser, createSoldier);
-router.put("/:id", adminOrSuperuser, updateSoldier);
+router.get('/', getAllSoldiers);
+router.get('/:id', getSoldierById);
+router.post('/', adminOrSuperuser, createSoldier);
+router.put('/:id', adminOrSuperuser, updateSoldier);
 
 // Routes accessible only by admin
-router.delete("/:id", adminOnly, deleteSoldier);
+router.delete('/:id', deleteSoldier);
 
 module.exports = router;
